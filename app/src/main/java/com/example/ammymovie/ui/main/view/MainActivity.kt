58b42932f35ac5badd335559fbeeec862b196344
amina1000 <amina1000@yandex.ru>
2021-07-21 17:38:56 +0300

    Выход в интернет. Урок 5

--- app/src/main/java/com/example/ammymovie/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/example/ammymovie/ui/main/view/MainActivity.kt
@@ -1,30 +1,76 @@
 package com.example.ammymovie.ui.main.view
 
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.view.Menu
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import com.example.ammymovie.R
 import com.example.ammymovie.databinding.MainActivityBinding
 import kotlinx.android.synthetic.main.main_activity.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.stream.Collectors
+import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: MainActivityBinding
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainActivityBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
         initNavigationMenu()
+        val handler = Handler() //Запоминаем основной поток
+        binding.ok.setOnClickListener {
+            try {
+                val uri = URL(binding.url.text.toString())
+                val handler = Handler() //Запоминаем основной поток
+                Thread {
+                    var urlConnection: HttpsURLConnection? = null
+                    try {
+                        urlConnection = uri.openConnection() as HttpsURLConnection
+                        urlConnection.requestMethod = "GET" //установка метода получения данных — GET
+                        urlConnection.readTimeout = 10000 //установка таймаута — 10 000 миллисекунд
+                        val reader =
+                            BufferedReader(InputStreamReader(urlConnection.inputStream)) //читаем данные в поток
+                        val result = getLines(reader)
+
+                        // Возвращаемся к основному потоку
+                         handler.post {
+                            binding.webview.loadDataWithBaseURL(null, result, "text/html; charset=utf-8", "utf-8", null)
+                         }
+                    } catch (e: Exception) {
+                        Log.e("Connect", "Fail connection", e)
+                        e.printStackTrace()
+                    } finally {
+                        urlConnection?.disconnect()
+                    }
+                }.start()
+            } catch (e: MalformedURLException) {
+                Log.e("Connect", "Fail URI", e)
+                e.printStackTrace()
+            }
+        }
         when (savedInstanceState) {
             null ->loadFragment(MainFragment.newInstance())
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun getLines(reader: BufferedReader): String {
+        return reader.lines().collect(Collectors.joining("\n"))
+    }
     private fun initNavigationMenu() {
         binding.navigation.setOnNavigationItemSelectedListener {
             when (it.itemId) {
